20252edf5ff268111f5102adf448c1baca987a25
Camilo Cabana <camilocabana.1104@gmail.com>
2022-01-26 18:57:40 +0900

    images and effects

--- SwiftUIProject/Views/Menu/MenuRowView.swift
+++ SwiftUIProject/Views/Menu/MenuRowView.swift
@@ -11,6 +11,11 @@ struct MenuRowView: View {
     var body: some View {
         HStack(alignment: .top, spacing: 15) {
             Image("1_100w")
+                
+//                .border(Color("G4"), width: 4)
+//                .cornerRadius(10)
+                .clipShape(Capsule())
+                .shadow(color: .black.opacity(0.5), radius: 5, x: 5, y: 5)
             VStack(alignment: .leading, spacing: 5) {
                 Text("Chicken Pizza")
                     .font(.title)
