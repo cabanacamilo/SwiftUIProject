4cd70520cbc85a8fb6021290650ab124836b9faa
Camilo Cabana <camilocabana.1104@gmail.com>
2022-02-03 18:24:50 +0900

    Add UIViewControllers

--- SwiftUIProject/Views/History/MapViewController.swift
+++ SwiftUIProject/Views/History/MapViewController.swift
@@ -10,21 +10,25 @@ import UIKit
 import MapKit
 
 class MapViewController: UIViewController {
+    
     var latitude:CLLocationDegrees = 40.8371
     var longitude:CLLocationDegrees = 14.2467
     var regionRadius:CLLocationDistance = 1000000
+    private let mapView = MKMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let mapView = MKMapView()
         mapView.frame = view.frame
+        view.addSubview(mapView)
+    }
+
+    func updateMap() {
         let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         mapView.setRegion(MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius),animated: true)
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
         mapView.addAnnotation(annotation)
-        view.addSubview(mapView)
     }
-
 
 }
 
